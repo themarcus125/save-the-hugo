821c29d8199a943ead407a46001e4581bcd0292c
Khoa Ngo <ngohakhoa@gmail.com>
2021-08-12 21:43:44 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## Giao
+snvv, bye
---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 8080;
+
+// sendFile will go here
+app.use(express.static(path.join(__dirname)));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/docs/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
