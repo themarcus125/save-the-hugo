d875ee8d7de3d0a164688d812bb10127b9721c16
Khoa Ngo <ngohakhoa@gmail.com>
2021-08-12 22:25:02 +0700

    Update serving path.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 8080;
 
 // sendFile will go here
-app.use(express.static(path.join(__dirname)));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/docs/index.html'));
